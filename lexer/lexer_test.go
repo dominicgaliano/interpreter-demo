c9bb01b92772e6de1a256c6c32bf67a29db81433
Dominic Galiano <dgaliano@utexas.edu>
2024-05-02 22:24:09 -0400

    Add REPL

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -69,6 +69,9 @@ return true;
 } else {
 return false;
 }
+
+10 == 10;
+10 != 9;
 `
 
 	tests := []struct {
@@ -140,6 +143,14 @@ return false;
 		{token.FALSE, "false"},
 		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
+        {token.INT, "10"},
+        {token.EQ, "=="},
+        {token.INT, "10"},
+        {token.SEMICOLON, ";"},
+        {token.INT, "10"},
+        {token.NOT_EQ, "!="},
+        {token.INT, "9"},
+        {token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
 
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/dominicgaliano/interpreter-demo/repl"
+)
+
+func main() {
+    user, err := user.Current()
+    if err != nil {
+        panic(err)
+    }
+    fmt.Printf("Welcome %s, this is the Monkey programming language!\n", user.Username)
+    fmt.Printf("Input commands below.\n")
+    repl.Start(os.Stdin, os.Stdout)
+}

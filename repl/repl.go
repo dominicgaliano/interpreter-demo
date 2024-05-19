ecef6914884e6ce3faba8b465883a974b34d23a7
Dominic Galiano <dgaliano@utexas.edu>
2024-05-19 14:26:08 -0400

    Modify REPL to use parser

--- repl/repl.go
+++ repl/repl.go
@@ -6,7 +6,7 @@ import (
 	"io"
 
 	"github.com/dominicgaliano/interpreter-demo/lexer"
-	"github.com/dominicgaliano/interpreter-demo/token"
+	"github.com/dominicgaliano/interpreter-demo/parser"
 )
 
 const PROMPT = ">> "
@@ -23,9 +23,22 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
-		}
+        program := p.ParseProgram()
+        if len(p.Errors()) != 0 {
+            printParserErrors(out, p.Errors())
+            continue
+        }
+
+        io.WriteString(out, program.String())
+        io.WriteString(out, "\n")
 	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+    io.WriteString(out, " parser errors:\n")
+    for _, error := range errors {
+        io.WriteString(out, "\t" + error + "\n")
+    }
 }

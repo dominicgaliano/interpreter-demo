37d81c88992c0f74b507f2a2a097d94a74ebbb60
Dominic Galiano <dgaliano@utexas.edu>
2024-05-19 22:04:01 -0400

    Implement boolean expression evaluation

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,30 +5,47 @@ import (
 	"github.com/dominicgaliano/interpreter-demo/object"
 )
 
+// Singleton instances of NULL, TRUE, and FALSE to ensure a single shared
+// instance of these values. Provides a performance increase by reducing
+// the need to instantiate these instances more than once.
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 
 	// Statements
 	case *ast.Program:
-        return evalStatements(node.Statements)
-
+		return evalStatements(node.Statements)
 	case *ast.ExpressionStatement:
-        return Eval(node.Expression)
+		return Eval(node.Expression)
 
-	// Expression
+	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
 
 	return nil
 }
 
 func evalStatements(statements []ast.Statement) object.Object {
-    var result object.Object
+	var result object.Object
 
-    for _, stmt := range statements {
-        result = Eval(stmt)
-    }
+	for _, stmt := range statements {
+		result = Eval(stmt)
+	}
 
-    return result
+	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
